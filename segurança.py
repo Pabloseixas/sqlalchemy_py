39009941f8cbfdc1e9ecddb68de9a949f84dd72f
Pablo  Seixas <140279590+Pablo-seixas@users.noreply.github.com>
2023-12-22 00:37:30 -0300

    Update segurança.py

--- segurança.py
+++ segurança.py
@@ -2,10 +2,3 @@
 import os
 from sqlalchemy import create_engine
 
-def get_engine():
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASS')
-    db = os.getenv('DB_NAME')
-
-    engine = create_engine(f'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{db}')
-    return engine
